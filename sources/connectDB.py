7a1ce66e30f26f102aa1073e98f84d8632934990
devgovi <devgovine@gmail.com>
2022-06-01 21:14:36 -0500

    added function to connect the database.

---
+++ sources/connectDB.py
@@ -0,0 +1,13 @@
+# Govine-J
+# GITS
+# 2022-06-01
+import sqlite3
+from sqlite3 import Error
+
+
+def portpass_db_con() -> sqlite3.Connection:
+    try:
+        return sqlite3.connect('sources/data/portpass.db')
+    except Error as e:
+        with open('sources/log/error.log', 'a') as f:
+            f.writelines(f'{e}\n')
