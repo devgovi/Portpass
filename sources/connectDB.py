9ea5c9f32a24b990945b5f139bd03df03c941bda
devgovi <devgovine@gmail.com>
2022-06-03 16:03:32 -0500

    change the path to database

--- sources/connectDB.py
+++ sources/connectDB.py
@@ -7,7 +7,7 @@ from sqlite3 import Error
 
 def portpass_db_con() -> sqlite3.Connection:
     try:
-        return sqlite3.connect('sources/data/portpass.db')
+        return sqlite3.connect('sources/database/portpass.db')
     except Error as e:
         with open('sources/log/error.log', 'a') as f:
             f.writelines(f'{e}\n')
